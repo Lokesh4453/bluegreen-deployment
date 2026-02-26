9eee415149e83ef2117e8eae260d1190472d271d
Lokesh4453 <143326441+Lokesh4453@users.noreply.github.com>
2026-02-26 11:56:15 +0530

    Add user data for HTTP server setup in EC2 instance

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "blue_server" {
   ami           = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 (Mumbai)
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Blue Environment - Jenkins Deployment</h1>" > /var/www/html/index.html
+              EOF
+
   tags = {
     Name = "Blue-Server"
   }
